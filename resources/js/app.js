41b2c67613b1d9c056fcecf878dfc653664d3180
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2026-07-15 02:41:00 +0330

    feat: install flatpickr + jalali-moment for Jalali date picker

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
 import Alpine from 'alpinejs'
 import Sortable from 'sortablejs'
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.min.css'
+import moment from 'jalali-moment'
 
 window.Alpine = Alpine
 window.Sortable = Sortable
+window.flatpickr = flatpickr
+window.moment = moment
 Alpine.start()
